3627f66e443f518bacdbcfc45df703fd8205c67a
Yama <yamanoory47@gmail.com>
2026-06-12 16:10:11 +0200

    Aufgabe 3 fertig

--- script.js
+++ script.js
@@ -12,6 +12,37 @@ anmeldeButton.addEventListener("click", function() {
 
     else if (emailFeld.value == "") {
         alert("Bitte E-Mail eingeben!");
-    } 
-
+    }
 });
+
+let lauftext = document.getElementById("lauftext");
+let position = -500; // Startposition des Lauftexts (außerhalb des Bildschirms)
+function BewegeLauftext() {
+    position ++; 
+    if (position > 1000) {
+        position = -500; // zurücksetzen, damit der Text wieder von links kommt
+    }
+    lauftext.style.left = position + "px";
+}
+setInterval(BewegeLauftext, 10); // führe die Funktion alle 10ms aus 
+
+
+let bild = document.getElementById("zeitbild");
+let stunde = new Date().getHours();
+if (stunde < 9) {
+    bild.src = "Sonne.png"; // Morgens
+} else {
+    bild.src = "Mond.png"; // Abends
+}
+
+
+let uhr = document.getElementById("uhr");
+
+function zeigeUhr() {
+    let jetzt = new Date();
+    uhr.textContent = jetzt.toLocaleTimeString();
+
+    setTimeout(zeigeUhr, 1000);
+}
+
+zeigeUhr();
